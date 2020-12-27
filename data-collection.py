f6b6db5d8ada9284523b45cc22ca9165b7a65c15
wctse <wchak.tse@gmail.com>
2020-12-27 21:44:49 +0800

    corrected data-collection

--- data-collection.py
+++ data-collection.py
@@ -1,14 +1,13 @@
 import pandas as pd
-from googleapiclient.discovery import build
 from scraping import get_video_from_channel, scrape_channel_id
-
 from datetime import datetime
 
 key = open('api-key.txt', 'r').read()
+channels = ['UCYO_jab_esuFRV4b17AJtAw']
 
 if key == 'Insert your API key here':
     print('Please edit api-key.txt to your own API key.')
 else:
-    youtube = build('youtube', 'v3', developerKey=key)
-    channels = scrape_channel_id(['UCYO_jab_esuFRV4b17AJtAw'], depth=1)
-    channels.to_csv('data_' + datetime.now().strftime('%Y%m%d%H%M'))
+    channels += scrape_channel_id(['UCYO_jab_esuFRV4b17AJtAw'], depth=1)
+    videos = get_video_from_channel(key, channels, 5, 1000)
+    pd.DataFrame(videos).to_csv('data/data_' + datetime.now().strftime('%Y%m%d%H%M') + '.csv')
