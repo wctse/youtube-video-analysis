d9d5e7dd22dafcb3fa6fe2a0ec427e93b4985f3b
wctse <wchak.tse@gmail.com>
2021-01-09 21:41:56 +0800

    scraping.py

--- data-collection.py
+++ data-collection.py
@@ -5,27 +5,25 @@ import scraping
 import json
 from datetime import datetime
 
-import importlib
-importlib.reload(scraping)
-
 key = open('api-key.txt', 'r').read()
-channels = ['UCYO_jab_esuFRV4b17AJtAw']
 now = datetime.now().strftime('%Y%m%d_%H%M%S')
 now_dt = datetime.strptime(now, '%Y%m%d_%H%M%S')
-old_csv = 'data/csv/data_20210101_145809.csv'
+old_csv = 'data/csv/data_20210109_153153.csv'  # Change this
 
 # Find channels to scrape videos on
-if key == 'Insert your API key here':
-    print('Please edit api-key.txt to your own API key.')
-else:
-    channels += scraping.scrape_channel_ids(['UCYO_jab_esuFRV4b17AJtAw'], depth=6, write_to_file=True)
+if input('Type "yes" if you wish to scrape channel ids.').lower() == 'yes':
+    channels = ['UCYO_jab_esuFRV4b17AJtAw']
+    if key == 'Insert your API key here':
+        print('Please edit api-key.txt to your own API key.')
+    else:
+        channels += scraping.scrape_channel_ids(['UCYO_jab_esuFRV4b17AJtAw'], depth=6, write_to_file=True)
 
 # Scrape videos
 # with open('data/channels/channels_' + now + '.txt', 'r') as file:
 with open('data/channels/channels_20201229_182240.txt', 'r') as file:
     channels = file.read().splitlines()
 
-videos, scrape_time = scraping.get_video_from_channels(key, channels[966:],
+videos, scrape_time = scraping.get_video_from_channels(key, channels[3600:4400],
                                                        how_many_videos=10, subscriber_threshold=1000)
 
 with open('data/raw/' + now + '.json', 'a') as file:
@@ -33,7 +31,7 @@ with open('data/raw/' + now + '.json', 'a') as file:
 
 # Use option 1 for live processing, option 2 for delayed processing
 ## Option 1
-with open('data/raw/' + now + '.json', 'r') as file:
+with open('data/raw/' + now + '.json', 'r', encoding='utf-8') as file:
     raw = json.load(file)
 
 ## Option 2
@@ -46,5 +44,5 @@ df = pd.DataFrame(parsed)
 
 # Combine new csv with old csv
 df_old = pd.read_csv(old_csv, index_col=0)
-df_concat = pd.concat([df, df_old]).reset_index()
+df_concat = pd.concat([df_old, df]).reset_index()
 df_concat.to_csv('data/csv/data_' + now + '.csv')
